8681f1dd6e4b0d51444d141904ecd22b2dd0a6a3
Alexey Kirinyuk <alexey.kirinuk@yandex.ru>
2015-09-20 01:04:13 +0600

    Bag fixed. Единичная матрица создаётся без проблем

--- lab1_kirinyuk_slinko/lab1_kirinyuk_slinko/main.cpp
+++ lab1_kirinyuk_slinko/lab1_kirinyuk_slinko/main.cpp
@@ -4,27 +4,37 @@ using namespace std;
 
 int main()
 {
-	const int m = 10;
-	double x[] = {5, 5, 5, 5, 5, 5, 5, 5, 5, 10};
+	const int m = 3;
+	double x[] = {10,10,11};
 	double mat[m][m];
 	
 	for(int i = 0; i < m; i++)
 	{
 		for(int j = 0; j < m; j++)
 		{
-			mat[j][i] = rand() % 100;
-			cout<<mat[j][i] << "\t";
+			mat[j][i] = rand() % 10;
 		}
-		cout<<endl;
 	}
+
+	for(int i = 0; i < m; i++)
+	{
+		for(int j = 0; j < m; j++)
+		{
+			cout<<mat[i][j]<<"\t";
+		}
+		cout<<"|"<<x[i]<<endl;
+	}
+
 	cout<<endl<<endl;
-	double b[10];
+
+	double b[m];
+
 	for(int i = 0; i < m; i++)
 	{
 		double h = 0;
-		for(int j = 0; j< m; j++)
+		for(int j = 0; j < m; j++)
 		{
-			h += mat[j][i] * x[j];
+			h += (mat[i][j] * x[i]);
 		}
 		b[i] = h;
 	}
@@ -34,65 +44,106 @@ int main()
 		cout<<b[i]<<"\t";
 	}
 	cout<<endl;
+
 	for(int k = 0; k < m; k++)
 	{
 		//нормируем, что бы первые все элементы были = 1
-		for(int i = 0; i < m; i++)
+		for(int i = k; i < m; i++)
 		{
 				double a = mat[i][k];
 				if(a != 0 && a != 1)
 				{
-					for(int j = k; j < m; j++)
+					for(int j = 0; j < m; j++)
 					{
 						mat[i][j] /= a;
 					}
+
 					b[i] /= a;
 				}
 		}
-
+		
 		for(int i = k + 1; i < m; i++)
 		{
-			
-			for(int j = 0; j < m; j++)
+			if(mat[i][k] != 0)
 			{
-				mat[i][j] -= mat[k][j];
+				for(int j = 0; j < m; j++)
+				{
+					mat[i][j] -= mat[k][j];
+				}
+				b[i] -= b[k];
 			}
-			b[i] -= b[k];
 		}
+		for(int i = 0; i < m; i++)
+	{
+		for(int j = 0; j < m; j++)
+		{
+			cout<<mat[i][j]<<"\t\t";
+		}
+		cout<<b[i]<<endl;
 	}
-
+		cout<<endl;
+		
+	}
+		
 	//обратно
 	for(int k = m - 1; k > 0; k--)
 	{
-		for(int i = m - 1; i >= 0; i--)
+		for(int i = k; i >= 0; i--)
 		{
 			double a = mat[i][k];
 			if(a != 0 && a != 1)
 			{
-				for(int j = k; j >= 0; j--)
+				for(int j = m-1; j >= 0; j--)
 				{
 					mat[i][j] /= a;
 				}
 				b[i] /= a;
 			}
 		}
+
 		for(int i = k - 1; i >= 0; i--)
 		{
-			for(int j = m - 1; j >=0; j--)
+			if(mat[i][k] != 0)
 			{
-				mat[i][j] -= mat[k][j];
+				for(int j = m - 1; j >=0; j--)
+				{
+					mat[i][j] -= mat[k][j];
+				}
+				b[i] -= b[k];
 			}
-			b[i] -= b[k];
 		}
+		for(int i = 0; i < m; i++)
+	{
+		for(int j = 0; j < m; j++)
+		{
+			cout<<mat[i][j]<<"\t\t";
+		}
+		cout<<b[i]<<endl;
+	}
+		cout<<endl;
 	}
 
-
-	cout<<endl<<endl<<endl;
 	for(int i = 0; i < m; i++)
 	{
 		for(int j = 0; j < m; j++)
 		{
-			cout<<mat[i][j]<<"\t";
+			cout<<mat[i][j]<<"\t\t";
+		}
+		cout<<b[i]<<endl;
+	}
+
+	for(int i = 0; i < m; i++)
+	{
+		b[i] /= mat[i][i];
+		mat[i][i] /= mat[i][i];
+	}
+
+	cout<<endl;
+	for(int i = 0; i < m; i++)
+	{
+		for(int j = 0; j < m; j++)
+		{
+			cout<<mat[i][j]<<"\t\t";
 		}
 		cout<<b[i]<<endl;
 	}
